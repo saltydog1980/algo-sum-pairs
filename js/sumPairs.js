0fd6a5276555e51d282251161df62b7aac8d75b0
saltydog1980 <mikeelambert99@gmail.com>
2022-05-26 21:08:46 -0400

    final code

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,19 @@
-exports.sumPairs = function() {
+exports.sumPairs = function(arr, num) {
+    ansArr = []
+    for (let i = 0; i < arr.length; i++) {
+    arr[i]
+        for (let j = 0; j < arr.length; j++) {
+            arr[j]
+            if(arr[i] + arr[j] === num) {
+                ansArr.push([arr[i], arr[j]])
+                arr[i] = -1
+                arr[j] = -1
+            }
+        }
+    }
 
+    if (ansArr.length === 0) {
+        return 'unable to find pairs'
+    } 
+    return ansArr      
 };
--- python/sum_pairs.py
+++ python/sum_pairs.py
@@ -1,1 +1,12 @@
-def sum_pairs():
+def sum_pairs(num_list, target):
+    nums = {}
+    answer_list = []
+
+    for num in num_list:
+        rem = target - num
+
+        if rem in nums:
+            answer_list.append([num, rem])
+        else:
+            nums[num] = True
+    return answer_list
