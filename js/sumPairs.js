ee5f3c690175143ed967e655a830c033e80df17d
saltydog1980 <mikeelambert99@gmail.com>
2022-05-27 09:45:35 -0400

    commented out the code

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,9 +1,10 @@
 exports.sumPairs = function(arr, num) {
+    //ans array to log match pairs
     ansArr = []
+    //nested for loop looping through summing each value to each other value in the array
+    //and then comparing to target, if match then pushing into ans array
     for (let i = 0; i < arr.length; i++) {
-    arr[i]
-        for (let j = 0; j < arr.length; j++) {
-            arr[j]
+        for (let j = 1; j < arr.length; j++) {
             if(arr[i] + arr[j] === num) {
                 ansArr.push([arr[i], arr[j]])
                 arr[i] = -1
@@ -11,9 +12,10 @@ exports.sumPairs = function(arr, num) {
             }
         }
     }
-
+//if nothing in ans array returning unable, else returning the ans arr
     if (ansArr.length === 0) {
         return 'unable to find pairs'
     } 
     return ansArr      
 };
+
--- python/sum_pairs.py
+++ python/sum_pairs.py
@@ -1,12 +1,16 @@
 def sum_pairs(num_list, target):
-    nums = {}
-    answer_list = []
-
-    for num in num_list:
-        rem = target - num
-
-        if rem in nums:
-            answer_list.append([num, rem])
-        else:
-            nums[num] = True
-    return answer_list
+    #ans list to log match pairs
+    ans_list = []
+    #nested for loop looping through summing each value to each other value in the list
+#   #and then comparing to target, if match then pushing into ans list
+    for i in range(0, len(num_list), 1):
+        for j in range(1, len(num_list), 1):
+            if num_list[i] + num_list[j] == target:
+                ans_list.append([num_list[i], num_list[j]])
+                num_list[i] = -1
+                num_list[j] = -1
+    #if nothing in ans list returning unable, else returning the ans list
+    if len(ans_list) == 0:
+        return 'unable to find pairs'
+    else:
+        return ans_list                
